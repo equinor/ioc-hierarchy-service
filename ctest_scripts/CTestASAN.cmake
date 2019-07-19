7a16d4f417ef77f0cf3fba2892277d383e3c5f64
Petter Moe Kvalvaag <pmkv@equinor>
2019-07-19 12:54:22 +0200

    Add CTest driver scripts

---
+++ ctest_scripts/CTestASAN.cmake
@@ -0,0 +1,32 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "Hierarchy service")
+set(CTEST_SITE "localhost")
+set(CTEST_BUILD_NAME "AddressSanitizer")
+ 
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_MEMORYCHECK_TYPE "AddressSanitizer")
+
+ctest_start(Experimental)
+
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "
+CMAKE_CXX_COMPILER=g++
+CMAKE_CXX_FLAGS=-g -O1 -fsanitize=address -fno-omit-frame-pointer
+")
+
+ctest_configure()
+ctest_build(FLAGS -j8)
+ctest_test()
+ctest_memcheck()
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "host.docker.internal:8080")
+set(CTEST_DROP_LOCATION "/submit.php?project=Hierarchy+service")
+set(CTEST_DROP_SITE_CDASH TRUE)
+ctest_submit()
---
+++ ctest_scripts/CTestTSAN.cmake
@@ -0,0 +1,33 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "Hierarchy service")
+set(CTEST_SITE "localhost")
+set(CTEST_BUILD_NAME "ThreadSanitizer")
+
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_MEMORYCHECK_TYPE "ThreadSanitizer")
+
+ctest_start(Experimental)
+
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "
+CMAKE_CXX_COMPILER=clang++-4.0
+CMAKE_CXX_FLAGS=-g -O1 -fsanitize=thread -fno-omit-frame-pointer -fPIC
+CMAKE_GENERATOR=Unix Makefiles
+")
+
+ctest_configure()
+ctest_build(FLAGS -j8)
+ctest_test()
+ctest_memcheck()
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "host.docker.internal:8080")
+set(CTEST_DROP_LOCATION "/submit.php?project=Hierarchy+service")
+set(CTEST_DROP_SITE_CDASH TRUE)
+ctest_submit()
