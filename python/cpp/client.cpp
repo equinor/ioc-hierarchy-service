d866fc41c09384ca178050a75a1c74a6585fa90e
Petter Moe Kvalvaag <pmkv@equinor.com>
2019-08-22 12:20:38 +0200

    Rewrite client to use lazy pirate pattern for resilience

--- python/cpp/client.cpp
+++ python/cpp/client.cpp
@@ -15,33 +15,75 @@
 #include <algorithm>
 #include <string>
 
+#define REQUEST_TIMEOUT 1500
+#define REQUEST_RETRIES 3
+
+namespace {
+namespace local {
+    std::shared_ptr<zmq::socket_t> get_socket (zmq::context_t& context, const std::string& app_mode) {
+        auto socket = std::make_shared<zmq::socket_t>(context, ZMQ_REQ);
+        if (app_mode == AppModeName.at(ApplicationMode::SERVER)) {
+            socket->connect ("tcp://127.0.0.1:5556");
+        }
+        else {
+            socket->connect ("tcp://127.0.0.1:5557");
+        }
+        const auto linger = int { 0 };
+        socket->setsockopt(ZMQ_LINGER, &linger, sizeof (linger));
+        return socket;
+    }
+}
+}
 
 std::vector<NodeType> query(std::vector<NodeType> in, std::string app_mode)
 {
     zmq::context_t context (1);
-    zmq::socket_t socket (context, ZMQ_REQ);
+    auto retries_left = int { REQUEST_RETRIES };
 
-    if (app_mode == AppModeName.at(ApplicationMode::SERVER)) {
-        socket.connect ("tcp://127.0.0.1:5556");
-    }
-    else {
-        socket.connect ("tcp://127.0.0.1:5557");
-    }
+    auto reply_list = std::vector<NodeType>();
+    auto socket = local::get_socket(context, app_mode);
 
-    std::ostringstream buffer;
+    auto buffer = std::ostringstream();
     {
         boost::archive::text_oarchive archive(buffer);
         archive << in;
     }
     zmq::message_t message((void*)buffer.str().c_str(), buffer.str().size()+1);
-    socket.send(message);
-    zmq::message_t reply;
-    socket.recv(&reply);
-    std::istringstream in_buffer(static_cast<char *>(reply.data()));
-    boost::archive::text_iarchive archive(in_buffer);
-    std::vector<NodeType> reply_list;
-    archive >> reply_list;
-    socket.close();
+
+    socket->send(message);
+
+    auto expect_reply = bool { true };
+    while (expect_reply){
+        zmq::pollitem_t items[] = {
+                {static_cast<void*>(*socket), 0 , ZMQ_POLLIN, 0}
+        };
+        zmq::poll(&items[0], 1, REQUEST_TIMEOUT);
+
+        // If there is a reply, process it
+        if (items[0].revents & ZMQ_POLLIN) {
+            zmq::message_t reply;
+            socket->recv(&reply);
+            std::istringstream in_buffer(static_cast<char *>(reply.data()));
+            boost::archive::text_iarchive archive(in_buffer);
+            archive >> reply_list;
+            socket->close();
+            expect_reply = false;
+        }
+        else if (--retries_left == 0) {
+            std::cout << "Error: hierarchy daemon does not reply" << std::endl;
+            expect_reply = false;
+            socket->close();
+            socket.reset();
+            reply_list.push_back({{{std::string("error"), std::string("daemon not responding")}}});
+        }
+        else {
+            std::cout << "Warning: no response from daemon, retrying" << std::endl;
+            socket->close();
+            socket = local::get_socket(context, app_mode);
+            socket->send(message);
+        }
+
+    }
     context.close();
     return reply_list;
 }
