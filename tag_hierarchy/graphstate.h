b3094e0e2d34bb356aadc8794e0d73d56c8817f4
Petter Moe Kvalvaag <pmkv@equinor.com>
2019-11-03 09:00:07 +0100

    Remove unused files

--- tag_hierarchy/graphstate.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-namespace GraphState { //TODO: stateless; make namespaced free functions
-    void Store(const std::string& serialized_graph);
-    std::string Retrieve();
-}
