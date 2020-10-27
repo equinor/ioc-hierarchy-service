4a45400450413eacdd8007accf55b7fe3ee30f87
Petter Moe Kvalvaag <pmkv@equinor.com>
2020-10-27 10:58:06 +0100

    Remove large test data file from header, compile in cpp
    These need to be split in order to not consume too much resources
    when building tests in parallell

--- tag_hierarchy/unittests/fixture.h
+++ tag_hierarchy/unittests/fixture.h
@@ -1,24 +1,7 @@
-#include "models/models.h"
-
-#include <boost/test/unit_test.hpp>
-
-#include "tag_hierarchy/tag_hierarchy.h"
+#pragma once
 
 struct Fixture {
-    Fixture() {
-        BOOST_TEST_MESSAGE( "Constructing test fixture" );
-        auto modelhierarchy = std::vector<NodeType>(
-#include "hierarchy_dump.cpp"
-        );
-        TagHierarchy::Handle(modelhierarchy);
-    }
-
-    ~Fixture() {
-        BOOST_TEST_MESSAGE( "teardown fixture" );
-        auto flush_command = std::vector<NodeType>(
-                {{{std::string("command"), std::string("flush")}}}
-                );
-        TagHierarchy::Handle(flush_command);
-    }
+  Fixture();
+  ~Fixture();
 };
 
