b1617f075801f572382bce7de38e2da498cb7ead
Petter Moe Kvalvaag <pmkv@equinor.com>
2020-10-29 13:08:47 +0100

    Use serialized hierarchy when running unit tests
    This makes use of the test data created by create_test_data, which is
    a dump of test hierarchy data in the boost::serialization format.

--- tag_hierarchy/unittests/fixture.cpp
+++ tag_hierarchy/unittests/fixture.cpp
@@ -1,15 +1,23 @@
 #include "tag_hierarchy/unittests/fixture.h"
 
+#include <fstream>
+
 #include <boost/test/unit_test.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/variant.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/variant.hpp>
 
 #include "models/models.h"
 #include "tag_hierarchy/tag_hierarchy.h"
 
 Fixture::Fixture() {
     BOOST_TEST_MESSAGE( "Constructing test fixture" );
-    auto modelhierarchy = std::vector<NodeType>(
-#include "hierarchy_dump.cpp"
-        );
+    auto modelhierarchy = std::vector<NodeType>();
+    std::ifstream input("unittests/serialized_hierarchy.dat");
+    boost::archive::text_iarchive ia(input);
+    ia >> modelhierarchy;
     TagHierarchy::Handle(modelhierarchy);
 }
 
