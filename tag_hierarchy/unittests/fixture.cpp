a109e09763165d868adc86b32c30432c45b1da68
Petter Kvalvaag <pmkv@equinor.com>
2021-09-02 12:54:47 +0200

    Use JSON for test data This was always a pain point, but now there is good support for json in boost so we can change strategy more easily now

--- tag_hierarchy/unittests/fixture.cpp
+++ tag_hierarchy/unittests/fixture.cpp
@@ -1,15 +1,45 @@
+#include <fstream>
 #include "tag_hierarchy/unittests/fixture.h"
 
 #include <boost/test/unit_test.hpp>
+#include <boost/json.hpp>
 
 #include "models/models.h"
 #include "tag_hierarchy/tag_hierarchy.h"
 
+
 Fixture::Fixture() {
     BOOST_TEST_MESSAGE( "Constructing test fixture" );
-    auto modelhierarchy = std::vector<NodeType>(
-#include "hierarchy_dump.cpp"
-        );
+    std::ifstream ifs("hierarchy_dump.json");
+    std::string content((std::istreambuf_iterator<char>(ifs)),
+                        (std::istreambuf_iterator<char>()));
+    boost::json::array modelhierarchy_json = boost::json::parse(content).as_array();
+    auto modelhierarchy = std::vector<NodeType>{};
+    for (const auto& value : modelhierarchy_json) {
+        auto temp = NodeType{};
+        const auto object = boost::json::value_to<boost::json::object>(value);
+
+        for (auto iter = object.cbegin(); iter != object.cend(); ++iter) {
+            auto value = iter->value();
+            if (value.is_null()) {
+                temp.insert({std::string(iter->key()), pybind11::none()});
+            }
+            else if (value.is_string()) {
+                temp.insert({std::string(iter->key()), boost::json::value_to<std::string>(iter->value())});
+            }
+            else if (value.is_double()) {
+                temp.insert({std::string(iter->key()), boost::json::value_to<double>(iter->value())});
+            }
+            else if (value.is_bool()) {
+                temp.insert({std::string(iter->key()), boost::json::value_to<bool>(iter->value())});
+            }
+            else if (value.is_int64()) {
+                temp.insert({std::string(iter->key()), boost::json::value_to<int>(iter->value())});
+            }
+        }
+        modelhierarchy.push_back(temp);
+    }
+    
     TagHierarchy::Handle(modelhierarchy);
 }
 
