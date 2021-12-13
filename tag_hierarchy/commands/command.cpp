d5f35f55c229f16e417554bfca43115a4f9c16ea
Petter Kvalvaag <pmkv@equinor.com>
2021-12-13 21:43:16 +0100

    Remove opencensus library
    Should upgrade to sidecar logging via dapr or equivalent,
    or maybe opentelemetry directly, if that serves the purpose
    better

--- tag_hierarchy/commands/command.cpp
+++ tag_hierarchy/commands/command.cpp
@@ -5,39 +5,29 @@
 #include "tag_hierarchy/commands/command.h"
 #include "tag_hierarchy/tag_hierarchy.h"
 
-#include <opencensus/trace/span.h>
-#include <opencensus/trace/with_span.h>
-#include <opencensus/context/with_context.h>
-
 Command::Command(std::string name) : name_(name) {
     TagHierarchy::Register(*this, name);
 }
 
 std::vector<NodeType>
 Command::Process(std::vector<NodeType>& request) {
-    static opencensus::trace::AlwaysSampler sampler;
     auto result = std::vector<NodeType>();
-    auto span = opencensus::trace::Span::StartSpan(name_, nullptr, &sampler);
-    span.AddAnnotation("Start running command " + name_);
     {
-        opencensus::trace::WithSpan ws(span);
         try {
             result = ProcessRequest(request);
-            span.AddAnnotation("Finished running command");
             if (!result.empty() &&
                 result.at(0).count("error")) {
                 const auto log_string = std::string("The ") + name_ +
                                         std::string(" command failed with the error ") +
                                         boost::get<std::string>(result.at(0).at("error"));
-                span.AddAnnotation(log_string);
+                std::cout << log_string << std::endl;
             }
         }
         catch (std::exception e) {
             const auto log_string = std::string("Command threw exception: ") + e.what();
-            span.AddAnnotation(log_string);
+            std::cout << log_string << std::endl;
         }
     }
-    span.End();
     return result;
 }
 
